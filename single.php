34e10987bc50bd4564dcfd99a7873e0435747f4d
HouseholdVTuber <householdvtuber@gmail.com>
2022-09-13 23:37:09 -0400

    rudimentary search functioning
    -made search.php display results, TODO style
    
    -make sidebar column stack on top of blog content in small view. TODO hide that on single blog posts to save real estate on mobile? hidden-xs?
    
    adjust body top padding so navbar doesn't cover header banner

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 <div class="container pt-5 pb-5">
     <section class="row">
         
-        <div class="col-lg-9">    
+        <div class="col-lg-9 col-md-8 order-last order-md-first order-lg-first pr-4">    
             <h1><?php the_title();?></h1>
 
             <?php if(has_post_thumbnail()):?>
@@ -14,7 +14,7 @@
                 <?php the_content();?>
             <?php endwhile; endif;?>
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-3 col-md-4 order-first order-md-last order-lg-last pl-4 pr-2">
         
             <?php if(is_active_sidebar('page-sidebar') ):?>
                 <?php get_sidebar('page');?>
